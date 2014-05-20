017c64f651932c2d78afed95b64a91c50fbd5154
Louise <louiselai88@gmail.com>
2014-05-20 17:55:05 +0100

    first validity test passes. Cannot be created without a name

--- my_yelp/spec/models/restaurant_spec.rb
+++ my_yelp/spec/models/restaurant_spec.rb
@@ -1,4 +1,11 @@
 require 'spec_helper'
 
 describe Restaurant do
+
+	it "is not valid without a name" do
+		restaurant = Restaurant.new(name: nil)
+		expect(restaurant).to have(1).error_on(:name)
+	end
+
+
 end
