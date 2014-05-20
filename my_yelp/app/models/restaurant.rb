d61904ff83605a52d9c87890555ac321eda45c29
Louise <louiselai88@gmail.com>
2014-05-20 19:57:28 +0100

    all (global) tests pass. Validations and mandatory input both implemented successfully.

--- my_yelp/app/models/restaurant.rb
+++ my_yelp/app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ActiveRecord::Base
-	validates :name, presence: true, format: {with: /\A[A-Z]/}
+	validates :name, presence: true, format: {with: /\A[A-Z]/, message: 'Name must start with a capital letter'}
 	validates :address, presence: true, length: {minimum: 3}
 	validates :cuisine, presence: true
 end
