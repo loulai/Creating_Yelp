90024e825316fb4ca1e81bca5ba3b78d11b1fad6
Louise <louiselai88@gmail.com>
2014-05-20 18:02:57 +0100

    bar one, all validations pass. About to implement that now, it requires a reg exp

--- my_yelp/app/models/restaurant.rb
+++ my_yelp/app/models/restaurant.rb
@@ -1,3 +1,5 @@
 class Restaurant < ActiveRecord::Base
 	validates :name, presence: true
+	validates :address, presence: true, length: {minimum: 3}
+	validates :cuisine, presence: true
 end
--- my_yelp/spec/models/restaurant_spec.rb
+++ my_yelp/spec/models/restaurant_spec.rb
@@ -7,5 +7,26 @@ describe Restaurant do
 		expect(restaurant).to have(1).error_on(:name)
 	end
 
+	it "is not valid without an address" do
+		restaurant = Restaurant.new(address: nil)
+		expect(restaurant).to have(2).error_on(:address)
+	end
+
+it "is not valid without a cuisine" do
+		restaurant = Restaurant.new(cuisine: nil)
+		expect(restaurant).to have(1).error_on(:cuisine)
+	end
+
+it "is not valid unless the first letter is a capital" do
+		restaurant = Restaurant.new(name: "abokado")
+		expect(restaurant).to have(1).error_on(:name)
+	end
+
+it "is not valid unless the address has three or more characters" do
+		restaurant = Restaurant.new(address: "rd")
+		expect(restaurant).to have(1).error_on(:address)
+	end
+
+
 
 end
