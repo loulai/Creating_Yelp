5232091e2d4a030d0f16a3f345e5598e86bdeee5
Louise <louiselai88@gmail.com>
2014-05-21 21:09:40 +0100

    added star ratings to views

--- my_yelp/app/helpers/reviews_helper.rb
+++ my_yelp/app/helpers/reviews_helper.rb
@@ -1,7 +1,7 @@
 module ReviewsHelper
 	def star_rating(value)
 		return value unless value.respond_to? :round
-			rounded_rating = rating_number.round
+			rounded_rating = value.round
 			filled_stars = '★' * rounded_rating
 			blank_stars = '☆' * (5 - rounded_rating)
 			result = filled_stars + blank_stars
