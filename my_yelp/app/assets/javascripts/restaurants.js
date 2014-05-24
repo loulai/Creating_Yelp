d6a46057b77352eab7a7300810892abdd7111a7a
Louise <louiselai88@gmail.com>
2014-05-24 19:53:50 +0100

    finished most of the functionality according to class

--- my_yelp/app/assets/javascripts/restaurants.js
+++ my_yelp/app/assets/javascripts/restaurants.js
@@ -2,11 +2,15 @@ $(document).ready(function() {
 	$('.new_review').on('submit', function(event){
 		event.preventDefault();
 		var reviewList = $(this).siblings('ul');
+		var currentRestaurant = $(this).parent();
 
 		$.post($(this).attr('action'), $(this).serialize(), function(response) {
-			var template = $('#review_template').html();
-			var newReview = Mustache.render(template, response); 
-			reviewList.appeend(newReview);
+			var newReview = Mustache.render($('#new_review').html(), response); 
+			reviewList.append(newReview);
+
+			currentRestaurant.find('.review_count').text(review.restaurant.review_count);
+			currentRestaurant.find('.average_rating_number').text(review.restaurant.average_rating);
+			currentRestaurant.find('.average_rating_stars').text(review.restaurant.average_rating_stars);
 		});
 	})
 });
